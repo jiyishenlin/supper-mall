05144bc7d551111c91ea9f2c31eded7e99d27d47
jiyishenlin <wangjisheng88@126.com>
2020-04-05 17:21:27 +0800

    '引入tab-bar，应用框架搭建完成'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick()">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="styleActive"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props: {
+      path:String,
+      activeColor:{
+        Type:String,
+        default:'#d4237a'
+      }
+    },
+    data(){
+      return {
+
+      }
+    },
+    computed:{/*计算属性是属性，使用时不用加小括号*/
+      isActive(){
+        return this.$route.path.indexOf(this.path) !== -1/*$route指向当前活跃的路由*/
+      },
+      styleActive(){
+        return this.isActive ? {color:this.activeColor} : {}/*动态绑定样式*/
+      }
+    },
+    methods:{
+      itemClick(){
+        this.$router.replace(this.path)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;/*公司常用高度*/
+    font-size: 12px;
+  }
+  .tab-bar-item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    margin-bottom: 2px;
+    vertical-align: middle;
+  }
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+const Home = () => import("../views/home/Home");/*懒加载方式*/
+const Category = () => import("../views/category/Category");/*懒加载方式*/
+const Cart = () => import("../views/cart/Cart");/*懒加载方式*/
+const Profile = () => import("../views/profile/Profile");/*懒加载方式*/
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+const router = new VueRouter({
+  routes,
+  mode:"history"
+})
+
+export default router
